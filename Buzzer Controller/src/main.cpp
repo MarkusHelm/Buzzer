5578c5dd2d87c06734d2a786d1a03a5b30e9d260
MarkusHelm <markus@hanshelm.de>
2023-11-01 23:21:38 +0100

    changed to HTTP

--- Buzzer Controller/src/main.cpp
+++ Buzzer Controller/src/main.cpp
@@ -1,106 +1,139 @@
 #include <Arduino.h>
-// OLED
-#include <U8x8lib.h>
-#include <Wire.h>
-// Wifi
-#include <esp_now.h>
 #include <WiFi.h>
+#include <WiFiAP.h>
+#include <HTTPClient.h>
+#include <U8x8lib.h> // OLED display
+#include <Wire.h>    // required by U8x8lib
 
-// Pin Setup
+// pin setup
 #define PIN_SW D8
 #define PIN_BTN_1 D3
 #define PIN_BTN_2 D2
 #define PIN_BTN_3 D1
 #define PIN_BAT A0
 
-// OLED Display Setup
+// OLED display setup
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);
 
-int buzzer1state = 0;
+// wifi setup
+#define WIFI_SSID "BuzzerNet"
+#define WIFI_PASSWORD "Hallo1234"
+WiFiServer server;
 
-// Buzzer 1 MAC  34:85:18:05:BF:10
-uint8_t broadcastAddress[] = {0x34, 0x85, 0x18, 0x05, 0xBF, 0x10};
+// controler setup
+IPAddress controllerIP(192, 168, 4, 1);
+IPAddress gateway(192, 168, 4, 1);
+IPAddress subnet(255, 255, 255, 0);
+#define CONTROLLER_PORT 80
 
-typedef struct buzzerMsg
+//  utility functions
+float getBatteryVoltage()
 {
-  int state;
-} buzzerMsg;
-
-typedef struct controllerMsg
-{
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-} controllerMsg;
-
-esp_now_peer_info_t peerInfo;
-
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
-{
-  buzzerMsg packet;
-  memcpy(&packet, incomingData, sizeof(packet));
-  buzzer1state = packet.state;
-  controllerMsg cmsg;
-  cmsg.r = 255 * buzzer1state;
-  cmsg.g = 255 * !buzzer1state;
-  cmsg.b = 0;
-  esp_now_send(broadcastAddress, (uint8_t *)&cmsg, sizeof(cmsg));
-}
-
-float getBatteryVoltage(void)
-{
+  // imit battery voltage measurement
   uint32_t Vbatt = 0;
+
+  // 16 measurements for better accuracy
   for (int i = 0; i < 16; i++)
   {
-    Vbatt = Vbatt + analogReadMilliVolts(PIN_BAT); // ADC with correction
+    // read battery voltage in mV
+    Vbatt = Vbatt + analogReadMilliVolts(PIN_BAT);
   }
-  float Vbattf = 2 * Vbatt / 16 / 1000.0; // attenuation ratio 1/2, mV --> V
-  return Vbattf;
+
+  // return calculate average (attention: ratio 1/2 and convert mV to V)
+  return 2 * Vbatt / 16 / 1000.0;
 }
 
-void setup(void)
+void setupWifiAP()
 {
-  // OLED Setup
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(controllerIP, gateway, subnet);
+  if (!WiFi.softAPConfig(controllerIP, gateway, subnet))
+  {
+    Serial.println("AP Config failed.");
+    return;
+  }
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+
+  server = WiFiServer(CONTROLLER_PORT);
+}
+void runServer()
+{
+  WiFiClient client = server.available();
+  if (client)
+  {
+    Serial.println("New client");
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        // get buzzerId, buttonPressed and batteryVoltage via query string
+        String buzzerId = "";
+        String buttonPressed = "";
+        String batteryVoltage = "";
+
+        // read first line of the request
+        String req = client.readStringUntil('\r');
+
+        Serial.println("request: " + req); // e.g. "GET /?buzzerId=1&buttonPressed=1&batteryVoltage=4.2&end HTTP/1.1
+
+        // parse query string
+        int buzzerIdStart = req.indexOf("buzzerId=");
+        int buzzerIdEnd = req.indexOf("&buttonPressed=");
+        buzzerId = req.substring(buzzerIdStart + 9, buzzerIdEnd);
+
+        int buttonPressedStart = req.indexOf("&buttonPressed=");
+        int buttonPressedEnd = req.indexOf("&batteryVoltage=");
+        buttonPressed = req.substring(buttonPressedStart + 15, buttonPressedEnd);
+
+        int batteryVoltageStart = req.indexOf("&batteryVoltage=");
+        int batteryVoltageEnd = req.indexOf("&end");
+        batteryVoltage = req.substring(batteryVoltageStart + 16, batteryVoltageEnd);
+
+        // create payload definition: color;brightness;mode; (e.g. 255,0,0;100;0;)
+        String payload = "";
+
+        // set color
+        payload += "255,0,0;";
+
+        // set brightness
+        payload += "100;";
+
+        // set mode
+        payload += "0;";
+
+        // send payload
+        client.println(payload);
+        break;
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+// setup method
+void setup()
+{
+  // OLED setup
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.setCursor(0, 0);
-  u8x8.println("Booting...");
+  u8x8.println("Starting up...");
 
-  Serial.begin(115200);
-
-  // Pin configuration
+  // pin configuration
   pinMode(PIN_SW, INPUT_PULLUP);
   pinMode(PIN_BTN_1, INPUT_PULLUP);
   pinMode(PIN_BTN_2, INPUT_PULLUP);
   pinMode(PIN_BTN_3, INPUT_PULLUP);
   pinMode(PIN_BAT, INPUT);
 
-  WiFi.mode(WIFI_STA);
-  esp_now_init();
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  esp_now_add_peer(&peerInfo);
-
-  u8x8.clear();
+  // setup wifi
+  setupWifiAP();
+  u8x8.println("Wifi setup done");
 }
 
-void loop(void)
+// loop method
+void loop()
 {
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0, 0);
-  u8x8.println("SW: " + String(!digitalRead(PIN_SW)));
-  u8x8.println("B1: " + String(!digitalRead(PIN_BTN_1)));
-  u8x8.println("B2: " + String(!digitalRead(PIN_BTN_2)));
-  u8x8.println("B3: " + String(!digitalRead(PIN_BTN_3)));
-  u8x8.println("BAT: " + String(getBatteryVoltage(), 1));
-  u8x8.println("BUZZER 1: " + String(!buzzer1state));
+  runServer();
 }
--- Buzzer/src/main.cpp
+++ Buzzer/src/main.cpp
@@ -1,99 +1,176 @@
 #include <Arduino.h>
-// LED Ring
-#include <Adafruit_NeoPixel.h>
-// Wifi
-#include <esp_now.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
 
-// Pin Setup
+// buzzer setup
+#define BUZZER_ID "2"
+
+// pin setup
 #define PIN_BTN D9
 #define PIN_NEO_PIXEL D10
 #define PIN_BAT A0
-#define NUM_PIXELS 12
 
-bool lastBtnState = HIGH;
+// led setup
+#define LED_PIXEL_COUNT 12
+#define LED_STROBE_SPEED 50
+// --------
+uint8_t ledColor[3] = {0, 0, 255};
+uint8_t ledBrightness = 50;
+uint8_t ledMode = 0; // 0=static, 1=strobe
+// --------
 
-Adafruit_NeoPixel NeoPixel(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel NeoPixel(LED_PIXEL_COUNT, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
 
-uint8_t broadcastAddress[] = {0x34, 0x85, 0x18, 0x91, 0xC9, 0x48}; // Controller
+// wifi setup
+#define WIFI_SSID "BuzzerNet"
+#define WIFI_PASSWORD "Hallo1234"
 
-typedef struct buzzerMsg
+// http setup
+uint8_t httpLastUpdateAt = 0;
+
+// controler setup
+#define CONTROLLER_IP "192.168.4.1"
+#define CONTROLLER_PORT 80
+
+//  utility functions
+float getBatteryVoltage()
 {
-  int state;
-} buzzerMsg;
-
-typedef struct controllerMsg
-{
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-} controllerMsg;
-
-esp_now_peer_info_t peerInfo;
-
-float getBatteryVoltage(void)
-{
+  // imit battery voltage measurement
   uint32_t Vbatt = 0;
+
+  // 16 measurements for better accuracy
   for (int i = 0; i < 16; i++)
   {
-    Vbatt = Vbatt + analogReadMilliVolts(PIN_BAT); // ADC with correction
+    // read battery voltage in mV
+    Vbatt = Vbatt + analogReadMilliVolts(PIN_BAT);
   }
-  float Vbattf = 2 * Vbatt / 16 / 1000.0; // attenuation ratio 1/2, mV --> V
-  return Vbattf;
+
+  // return calculate average (attention: ratio 1/2 and convert mV to V)
+  return 2 * Vbatt / 16 / 1000.0;
 }
 
-void setColor(uint8_t r, uint8_t g, uint8_t b)
+// TODO: TITUS lesen
+void showLED()
 {
-  const uint32_t color = NeoPixel.Color(r, g, b);
-  NeoPixel.fill(color, 0, NUM_PIXELS);
+  NeoPixel.setBrightness(ledBrightness);
+  switch (ledMode)
+  {
+  case 0: // static
+    NeoPixel.fill(NeoPixel.Color(ledColor[0], ledColor[1], ledColor[2]), 0, LED_PIXEL_COUNT);
+    break;
+  case 1: // strobe
+    if (millis() % (2 * LED_STROBE_SPEED) < LED_STROBE_SPEED)
+      NeoPixel.fill(NeoPixel.Color(ledColor[0], ledColor[1], ledColor[2]), 0, LED_PIXEL_COUNT);
+    else
+      NeoPixel.fill(NeoPixel.Color(0, 0, 0), 0, LED_PIXEL_COUNT);
+    break;
+  default: // led off
+    NeoPixel.fill(NeoPixel.Color(0, 0, 0), 0, LED_PIXEL_COUNT);
+    break;
+  }
   NeoPixel.show();
 }
-
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+void connectWifi()
 {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // check if WiFi is not connected to reconnect (or connect for the first time)
+  if (!WiFi.isConnected())
+  {
+    Serial.print("WiFi is not Connected.");
+    Serial.print("Connecting to WiFi ...");
+
+    // connect to WiFi
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println("Connected successfully to the WiFi network!");
+  }
+}
+void callController(boolean buttonPressed = false)
+{
+  // setup clients
+  WiFiClient client;
+  HTTPClient http;
+
+  // send http request
+  if (http.begin(client, "http://" + String(CONTROLLER_IP) + ":" + String(CONTROLLER_PORT) + "?buzzerId=" + String(BUZZER_ID) + "&buttonPressed=" + String(buttonPressed) + "&batteryVoltage=" + String(getBatteryVoltage()) + "&end"))
+  {
+    // get http code and payload
+    int httpCode = http.GET();
+
+    if (httpCode > 0 && httpCode == HTTP_CODE_OK)
+    {
+      // payload definition: color;brightness;mode; (e.g. 255,0,0;100;0;)
+      String payload = http.getString();
+
+      // split payload by ; and set payload split
+      char *payloadSplit = strtok((char *)payload.c_str(), ";");
+
+      // split first part of splitted payload by , and set color split
+      char *colorSplit = strtok(payloadSplit, ",");
+
+      // set color
+      ledColor[0] = atoi(colorSplit);
+      colorSplit = strtok(NULL, ",");
+      ledColor[1] = atoi(colorSplit);
+      colorSplit = strtok(NULL, ",");
+      ledColor[2] = atoi(colorSplit);
+
+      // set brightness
+      payloadSplit = strtok(NULL, ";");
+
+      // set mode
+      payloadSplit = strtok(NULL, ";");
+    }
+    else
+    {
+      Serial.println("Error on HTTP request (httpCode: " + String(httpCode) + ")");
+    }
+    http.end();
+  }
+
+  // update last http update
+  httpLastUpdateAt = millis();
 }
 
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
-{
-  controllerMsg packet;
-  memcpy(&packet, incomingData, sizeof(packet));
-  setColor(packet.r, packet.g, packet.b);
-}
-
+// setup function
 void setup()
 {
+  // setup pins
   pinMode(PIN_BTN, INPUT_PULLUP);
   pinMode(PIN_BAT, INPUT);
 
+  // setup led
   NeoPixel.begin();
-  NeoPixel.setBrightness(25);
-  setColor(0, 0, 10);
+  showLED();
 
+  // setup serial
   Serial.begin(115200);
 
-  // ESP-NOW Setup
-  WiFi.mode(WIFI_STA);
-  esp_now_init();
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  esp_now_add_peer(&peerInfo);
+  // call connect wifi to connect to wifi
+  connectWifi();
 }
 
+// loop function
 void loop()
 {
-  const bool btnState = digitalRead(PIN_BTN);
+  // show led
+  showLED();
+  // call connect wifi to reconnect if needed
+  connectWifi();
 
-  if (btnState != lastBtnState)
+  // check if last http update is older than 250ms and call controller
+  if (millis() - httpLastUpdateAt > 250)
   {
-    buzzerMsg packet;
-    packet.state = btnState;
-    esp_now_send(broadcastAddress, (uint8_t *)&packet, sizeof(packet));
+    callController();
   }
 
-  lastBtnState = btnState;
+  // if button press call controller
+  if (digitalRead(PIN_BTN) == LOW)
+  {
+    callController(true);
+  }
 }
